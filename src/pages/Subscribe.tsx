82f22e7eaea8cf9d8a5a52aa8bbc6556d1711880
Victor Rubiño <105171818+rubinoDev@users.noreply.github.com>
2022-06-27 20:19:27 -0300

    fixing background bug

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -2,6 +2,7 @@ import { Logo } from "../components/Logo";
 import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { useCreateSubscriberMutation } from "../graphql/generated";
+import bgImg from '../assets/code-mockup.png';
 
 export function Subscribe(){
 
@@ -68,7 +69,7 @@ export function Subscribe(){
         </div>
       </div>
       
-      <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
+      <img src={bgImg} className="mt-10" alt="" />
     </div>
   )
 }
